8365849cc55ea9bddce9fcb33c83145627c6889e
sta-ger <pascaldelger@gmail.com>
2019-05-11 18:15:31 +0200

    Default game session test

--- src/test/java/slotify4j/session/DefaultGameSessionTest.java
+++ src/test/java/slotify4j/session/DefaultGameSessionTest.java
@@ -2,21 +2,24 @@ package slotify4j.session;
 
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import static org.junit.Assert.*;
 
 public class DefaultGameSessionTest {
 
-    public void testDefaultSessionHasProperInitialValues(GameSession session, GameSessionConfig config) {
+    public void testDefaultSessionHasProperInitialValues(Constructor<? extends GameSession> sessionConstructor, Constructor<? extends GameSessionConfig> configConstructor) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        GameSessionConfig config = configConstructor.newInstance();
+        GameSession session = sessionConstructor.newInstance(config);
         assertEquals(session.getAvailableBets(), config.getAvailableBets());
         assertEquals(session.getBet(), config.getAvailableBets()[0]);
         assertEquals(session.getCreditsAmount(), 1000);
     }
 
     @Test
-    public void testCreateNewSession() {
-        GameSessionConfig config = new DefaultGameSessionConfig();
-        GameSession session = new DefaultGameSession(config);
-        testDefaultSessionHasProperInitialValues(session, config);
+    public void testCreateNewSession() throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        testDefaultSessionHasProperInitialValues(DefaultGameSession.class.getConstructor(GameSessionConfig.class), DefaultGameSessionConfig.class.getConstructor());
     }
 
 }
