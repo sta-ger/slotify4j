cd83feaeb29cb3fba93cc247b20c73c2aa8ede99
sta-ger <pascaldelger@gmail.com>
2019-05-20 09:07:39 +0200

    Win calculator

---
+++ src/main/java/slotify4j/session/videogames/reelgames/ReelGameSessionWinningLineModelImpl.java
@@ -0,0 +1,33 @@
+package slotify4j.session.videogames.reelgames;
+
+public class ReelGameSessionWinningLineModelImpl implements ReelGameSessionWinningLineModel {
+    @Override
+    public ReelGameSessionLinesDirectionData getDirection() {
+        return null;
+    }
+
+    @Override
+    public String getItemId() {
+        return null;
+    }
+
+    @Override
+    public String getLineId() {
+        return null;
+    }
+
+    @Override
+    public int[] getItemsPositions() {
+        return new int[0];
+    }
+
+    @Override
+    public int[] getWildItemsPositions() {
+        return new int[0];
+    }
+
+    @Override
+    public long getWinningAmount() {
+        return 0;
+    }
+}
