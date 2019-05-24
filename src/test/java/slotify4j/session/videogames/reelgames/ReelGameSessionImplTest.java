e03685a254b60c66c9ede1652b9cce0c84170f58
sta-ger <pascaldelger@gmail.com>
2019-05-24 08:49:22 +0200

    Reel game session

--- src/test/java/slotify4j/session/videogames/reelgames/ReelGameSessionImplTest.java
+++ src/test/java/slotify4j/session/videogames/reelgames/ReelGameSessionImplTest.java
@@ -3,8 +3,7 @@ package slotify4j.session.videogames.reelgames;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReelGameSessionImplTest {
 
@@ -20,5 +19,37 @@ public class ReelGameSessionImplTest {
         assertNull(session.getWinningLines());
         assertNull(session.getWinningScatters());
     }
+
+    public static void testPlayUntilWin(Constructor<? extends ReelGameSession> sessionConstructor, Constructor<? extends ReelGameSessionConfig> configConstructor) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        long lastBet;
+        long lastCredits;
+        boolean wasLinesWin;
+        boolean wasScattersWin;
+
+        ReelGameSessionConfig config = configConstructor.newInstance();
+        config.setCreditsAmount(10000000);
+        ReelGameSession session = sessionConstructor.newInstance(config);
+
+        int timesToPlay = 1000;
+        for (int i = 0; i < timesToPlay; i++) {
+            while (session.getWinningAmount() == 0 || wasLinesWin || wasScattersWin) {
+                wasLinesWin = false;
+                wasScattersWin = false;
+                lastCredits = session.getCreditsAmount();
+                lastBet = session.getBet();
+                session.play();
+                if (session.getWinningAmount() === 0) {
+                    assertEquals(session.getCreditsAmount(), lastCredits - lastBet);
+                }
+            }
+            assertTrue(session.getCreditsAmount() >= lastCredits - lastBet);
+
+            wasLinesWin = session.getWinningLines().size() > 0;
+            wasScattersWin = session.getWinningScatters().size() > 0;
+            if (i == timesToPlay - 1 && !wasLinesWin && !wasScattersWin) {
+                i = 0;
+            }
+        }
+    };
 
 }
