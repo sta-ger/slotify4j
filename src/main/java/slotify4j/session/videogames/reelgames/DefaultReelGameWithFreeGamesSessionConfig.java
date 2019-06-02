c44a2d50e3b1ccde3ec0fb777bfe4f8539ebdcc4
sta-ger <pascaldelger@gmail.com>
2019-06-02 10:35:21 +0200

    Reel game with free games session

--- src/main/java/slotify4j/session/videogames/reelgames/DefaultReelGameWithFreeGamesSessionConfig.java
+++ src/main/java/slotify4j/session/videogames/reelgames/DefaultReelGameWithFreeGamesSessionConfig.java
@@ -1,6 +1,6 @@
 package slotify4j.session.videogames.reelgames;
 
-public class ReelGameWithFreeGamesSessionConfigImpl implements ReelGameWithFreeGamesSessionConfig {
+public class DefaultReelGameWithFreeGamesSessionConfig implements ReelGameWithFreeGamesSessionConfig {
 
     @Override
     public int getFreeGamesForScatters(String itemId, int numberOfItems) {
