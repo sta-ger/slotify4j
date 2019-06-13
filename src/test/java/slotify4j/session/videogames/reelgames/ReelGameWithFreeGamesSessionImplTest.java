5d1b6fa2c6d155565afee6f54b900669ab9b85bc
sta-ger <pascaldelger@gmail.com>
2019-06-13 09:23:30 +0200

    Reel game with free games session

--- src/test/java/slotify4j/session/videogames/reelgames/ReelGameWithFreeGamesSessionImplTest.java
+++ src/test/java/slotify4j/session/videogames/reelgames/ReelGameWithFreeGamesSessionImplTest.java
@@ -1,5 +1,10 @@
 package slotify4j.session.videogames.reelgames;
 
+import slotify4j.session.videogames.reelgames.reelscontroller.ReelGameSessionReelsControllerImpl;
+import slotify4j.session.videogames.reelgames.wincalculator.ReelGameSessionWinCalculatorImpl;
+
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReelGameWithFreeGamesSessionImplTest {
@@ -26,6 +31,24 @@ public class ReelGameWithFreeGamesSessionImplTest {
         }
         assertEquals(session.getFreeGameNum(), 0);
         assertEquals(session.getFreeGameSum(), 0);
+    }
+
+    public static ReelGameWithFreeGamesSessionConfig createConfigForTestPlayFreeGames() {
+        DefaultReelGameWithFreeGamesSessionConfig conf = new DefaultReelGameWithFreeGamesSessionConfig();
+        conf.setReelsItemsSequences(ReelGameSessionReelsControllerImpl.createItemsSequences(conf.getReelsNumber(), conf.getAvailableItems(), new HashMap<>(){{
+            put(0, new HashMap<>(){{
+                put("S", 0);
+            }});
+            put(4, new HashMap<>(){{
+                put("S", 0);
+            }});
+        }});
+        return conf;
+    }
+
+    public static ReelGameWithFreeGamesSession createSessionForTestPlayFreeGames() {
+        ReelGameWithFreeGamesSessionConfig conf = createConfigForTestPlayFreeGames();
+        return new ReelGameWithFreeGamesSessionImpl(conf, new ReelGameSessionReelsControllerImpl(conf), new ReelGameSessionWinCalculatorImpl(conf));
     }
 
 }
