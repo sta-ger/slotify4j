7b19cdbaf715dbbeaf16051ce962ccb2f618bad9
sta-ger <pascaldelger@gmail.com>
2019-06-15 11:39:10 +0200

    Test play free games

--- src/test/java/slotify4j/session/videogames/reelgames/ReelGameWithFreeGamesSessionImplTest.java
+++ src/test/java/slotify4j/session/videogames/reelgames/ReelGameWithFreeGamesSessionImplTest.java
@@ -1,12 +1,15 @@
 package slotify4j.session.videogames.reelgames;
 
+import org.junit.jupiter.api.Test;
 import slotify4j.session.videogames.reelgames.reelscontroller.ReelGameSessionReelsController;
 import slotify4j.session.videogames.reelgames.reelscontroller.ReelGameSessionReelsControllerImpl;
 import slotify4j.session.videogames.reelgames.wincalculator.ReelGameSessionWinCalculatorImpl;
 
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ReelGameWithFreeGamesSessionImplTest {
 
@@ -36,20 +39,69 @@ class ReelGameWithFreeGamesSessionImplTest {
 
     public static ReelGameWithFreeGamesSessionConfig createConfigForTestPlayFreeGames() {
         DefaultReelGameWithFreeGamesSessionConfig conf = new DefaultReelGameWithFreeGamesSessionConfig();
-        conf.setReelsItemsSequences(ReelGameSessionReelsController.createItemsSequences(conf.getReelsNumber(), conf.getAvailableItems(), new HashMap<>(){{
-            put(0, new HashMap<>(){{
+        conf.setReelsItemsSequences(ReelGameSessionReelsController.createItemsSequences(conf.getReelsNumber(), conf.getAvailableItems(), new HashMap<>() {{
+            put(0, new HashMap<>() {{
                 put("S", 0);
             }});
-            put(4, new HashMap<>(){{
+            put(4, new HashMap<>() {{
                 put("S", 0);
             }});
         }}));
         return conf;
     }
 
-    public static ReelGameWithFreeGamesSession createSessionForTestPlayFreeGames() {
+    @Test
+    void testPlayFreeGames() throws Exception {
         ReelGameWithFreeGamesSessionConfig conf = createConfigForTestPlayFreeGames();
-        return new ReelGameWithFreeGamesSessionImpl(conf, new ReelGameSessionReelsControllerImpl(conf), new ReelGameSessionWinCalculatorImpl(conf));
+        ReelGameWithFreeGamesSession session = new ReelGameWithFreeGamesSessionImpl(conf, new ReelGameSessionReelsControllerImpl(conf), new ReelGameSessionWinCalculatorImpl(conf));
+
+        //The following situations need to be checked:
+        boolean wasNormalFreeGames = false; //played normal 10 free games
+        boolean wasAdditionalFreeGames = false; //free games was won again at free games mode
+        boolean wasFreeBank = false; //was any winning during free games mode
+        boolean wasNoFreeBank = false; //was no winnings during free games mode
+        while (!wasNormalFreeGames || !wasAdditionalFreeGames || !wasFreeBank || !wasNoFreeBank) {
+            while (session.getFreeGameSum() == 0 || (session.getFreeGameSum() > 0 && session.getFreeGameNum() == session.getFreeGameSum())) { //Play until won free games
+                session.play();
+            }
+            int playedFreeGamesCount = 0;
+            int expectedPlayedFreeGamesCount = session.getFreeGameSum();
+            long lastFreeBank = 0;
+            long creditsBeforeFreeGame = session.getCreditsAmount();
+            while (session.getFreeGameSum() > 0 && session.getFreeGameNum() != session.getFreeGameSum()) { //Play until end of free games
+                lastFreeBank = session.getFreeGameBank();
+                session.play();
+                assertEquals(session.getFreeGameBank(), lastFreeBank + session.getWinningAmount());
+                assertEquals(session.getCreditsAmount(), creditsBeforeFreeGame); //Bet should not be subtracted at free games mode
+                playedFreeGamesCount++;
+                if (session.getFreeGameSum() > expectedPlayedFreeGamesCount) {
+                    wasAdditionalFreeGames = true;
+                    expectedPlayedFreeGamesCount = session.getFreeGameSum();
+                    assertTrue(session.getWinningScatters().size() > 0);
+                    Stream.of(conf.getScattersData()).forEach(scatterData -> {
+                        if (conf.getFreeGamesForScatters(scatterData.getItemId(), scatterData.getMinimumItemsNumForScatterWin()) > 0) {
+                            assertTrue(session.getWinningScatters().containsKey(scatterData.getItemId()));
+                            assertTrue(conf.getFreeGamesForScatters(scatterData.getItemId(), session.getWinningScatters().get(scatterData.getItemId()).getItemsPositions().length) > 0);
+                            assertEquals(session.getWonFreeGamesNumber(), conf.getFreeGamesForScatters(scatterData.getItemId(), session.getWinningScatters().get(scatterData.getItemId()).getItemsPositions().length));
+                        }
+                    });
+                } else {
+                    wasNormalFreeGames = true;
+                }
+            }
+
+            if (lastFreeBank == 0) {
+                wasNoFreeBank = true;
+            } else {
+                wasFreeBank = true;
+            }
+
+            assertEquals(session.getCreditsAmount(), creditsBeforeFreeGame + lastFreeBank);
+            assertEquals(playedFreeGamesCount, expectedPlayedFreeGamesCount);
+        }
     }
+
+    ;
+
 
 }
