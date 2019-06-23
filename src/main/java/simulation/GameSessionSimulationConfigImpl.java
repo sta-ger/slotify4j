69be57a246b7897c8f8db307285b165da5867d1d
sta-ger <pascaldelger@gmail.com>
2019-06-23 21:43:41 +0200

    Simulation

---
+++ src/main/java/simulation/GameSessionSimulationConfigImpl.java
@@ -0,0 +1,24 @@
+package simulation;
+
+public class GameSessionSimulationConfigImpl implements GameSessionSimulationConfig {
+
+    @Override
+    public void setNumberOfRounds(long value) {
+
+    }
+
+    @Override
+    public long getNumberOfRounds() {
+        return 0;
+    }
+
+    @Override
+    public void setChangeBetScenario(ChangeBetScenario changeBetScenario) {
+
+    }
+
+    @Override
+    public ChangeBetScenario getChangeBetScenario() {
+        return null;
+    }
+}
