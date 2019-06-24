5945cec970cc4f66fbf85fe2ac4cbe945d0c64df
sta-ger <pascaldelger@gmail.com>
2019-06-24 08:46:13 +0200

    Simulation

--- src/main/java/simulation/GameSessionSimulationConfigImpl.java
+++ src/main/java/simulation/GameSessionSimulationConfigImpl.java
@@ -1,24 +1,26 @@
 package simulation;
 
 public class GameSessionSimulationConfigImpl implements GameSessionSimulationConfig {
+    private long numberOfRounds = 1000;
+    private ChangeBetScenario changeBetScenario = ChangeBetScenario.DontChange;
 
     @Override
     public void setNumberOfRounds(long value) {
-
+        numberOfRounds = value;
     }
 
     @Override
     public long getNumberOfRounds() {
-        return 0;
+        return numberOfRounds;
     }
 
     @Override
     public void setChangeBetScenario(ChangeBetScenario changeBetScenario) {
-
+        this.changeBetScenario = changeBetScenario;
     }
 
     @Override
     public ChangeBetScenario getChangeBetScenario() {
-        return null;
+        return changeBetScenario;
     }
 }
