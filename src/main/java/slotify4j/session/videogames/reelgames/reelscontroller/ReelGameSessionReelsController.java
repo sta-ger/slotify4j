eb1ebda3736829cbcd51025f052372314e54bdcc
Stanislau Herasimovich <stanislau@e-gaming.cz>
2019-06-27 19:10:52 +0200

    Fix

--- src/main/java/slotify4j/session/videogames/reelgames/reelscontroller/ReelGameSessionReelsController.java
+++ src/main/java/slotify4j/session/videogames/reelgames/reelscontroller/ReelGameSessionReelsController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
-import java.util.HashMap;
 
 public interface ReelGameSessionReelsController {
 
