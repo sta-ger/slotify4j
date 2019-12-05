43e33aaff503af32b04d98667bbf6c87fe5402eb
sta-ger <pascaldelger@gmail.com>
2019-12-05 09:40:08 +0100

    Simulation change bet strategy

--- src/main/java/slotify4j/simulation/GameSessionSimulationConstantChangeBetStrategy.java
+++ src/main/java/slotify4j/simulation/GameSessionSimulationConstantChangeBetStrategy.java
@@ -3,15 +3,12 @@ package slotify4j.simulation;
 import slotify4j.session.GameSession;
 
 import java.util.Arrays;
-import java.util.Random;
 
-public class GameSessionSimulationRandomChangeBetStrategy implements GameSessionSimulationChangeBetStrategy {
+public class GameSessionSimulationConstantChangeBetStrategy implements GameSessionSimulationChangeBetStrategy {
 
     @Override
     public void setBetForPlay(GameSession session) {
         long[] bets = session.getAvailableBets();
-        long bet = bets[new Random().nextInt(bets.length)];
-        session.setBet(bet);
         if (session.canPlayNextGame()) {
             Arrays.sort(bets);
             session.setBet(bets[0]);
