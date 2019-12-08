b999d8389ec29819ae5fe107cfc261429d84c5e7
sta-ger <pascaldelger@gmail.com>
2019-12-08 19:56:08 +0100

    Random change bet simulation strategy

--- src/main/java/slotify4j/simulation/GameSessionSimulationRandomChangeBetStrategy.java
+++ src/main/java/slotify4j/simulation/GameSessionSimulationRandomChangeBetStrategy.java
@@ -2,7 +2,6 @@ package slotify4j.simulation;
 
 import slotify4j.session.GameSession;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class GameSessionSimulationRandomChangeBetStrategy implements GameSessionSimulationChangeBetStrategy {
@@ -12,10 +11,6 @@ public class GameSessionSimulationRandomChangeBetStrategy implements GameSession
         long[] bets = session.getAvailableBets();
         long bet = bets[new Random().nextInt(bets.length)];
         session.setBet(bet);
-        if (session.canPlayNextGame()) {
-            Arrays.sort(bets);
-            session.setBet(bets[0]);
-        }
     }
 
 }
---
+++ src/test/java/slotify4j/simulation/GameSessionSimulationRandomChangeBetStrategyTest.java
@@ -0,0 +1,51 @@
+package slotify4j.simulation;
+
+import org.junit.jupiter.api.Test;
+import slotify4j.session.UnableToPlayException;
+import slotify4j.session.videogames.reelgames.DefaultReelGameSessionConfig;
+import slotify4j.session.videogames.reelgames.ReelGameSessionConfig;
+import slotify4j.session.videogames.reelgames.ReelGameSessionImpl;
+import slotify4j.session.videogames.reelgames.reelscontroller.ReelGameSessionReelsController;
+import slotify4j.session.videogames.reelgames.reelscontroller.ReelGameSessionReelsControllerImpl;
+import slotify4j.session.videogames.reelgames.wincalculator.ReelGameSessionWinCalculator;
+import slotify4j.session.videogames.reelgames.wincalculator.ReelGameSessionWinCalculatorImpl;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameSessionSimulationRandomChangeBetStrategyTest {
+
+    @Test
+    public void setBetForPlay() throws UnableToPlayException {
+        ReelGameSessionConfig sessionConfig = new DefaultReelGameSessionConfig();
+        ReelGameSessionReelsController reelsController = new ReelGameSessionReelsControllerImpl(sessionConfig);
+        ReelGameSessionWinCalculator winningCalculator = new ReelGameSessionWinCalculatorImpl(sessionConfig);
+        ReelGameSessionImpl session = new ReelGameSessionImpl(sessionConfig, reelsController, winningCalculator);
+
+        GameSessionSimulationConfig simulationConfig = DefaultGameSessionSimulationConfig
+                .builder()
+                .withChangeBetStrategy(new GameSessionSimulationRandomChangeBetStrategy())
+                .build();
+        GameSessionSimulation simulation = new GameSessionSimulationImpl(session, simulationConfig);
+
+        HashSet<Long> playedBets = new HashSet<>();
+        simulation.setBeforePlayCallback(() -> session.setCreditsAmount(1000000));
+        simulation.setAfterPlayCallback(() -> playedBets.add(session.getBet()));
+
+        simulation.run();
+
+        assertEquals(playedBets.size(), session.getAvailableBets().length);
+
+        // After simulation playedBets should contain shuffled set of bets
+        assertFalse(
+                Arrays.equals(
+                        session.getAvailableBets(),
+                        playedBets.stream().mapToLong(Number::longValue).toArray()
+                )
+        );
+
+    }
+
+}
