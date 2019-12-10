079c961fde1757e31bb1adc3ea4510481f2b92a9
Stanislav Gerasimovich <pascaldelger@gmail.com>
2019-12-10 18:17:26 +0100

    Simulation random change bet strategy test

--- src/test/java/slotify4j/simulation/RandomChangeBetStrategyTest.java
+++ src/test/java/slotify4j/simulation/RandomChangeBetStrategyTest.java
@@ -1,51 +1,91 @@
 package slotify4j.simulation;
 
 import org.junit.jupiter.api.Test;
+import slotify4j.session.GameSession;
 import slotify4j.session.UnableToPlayException;
-import slotify4j.session.videogames.reelgames.DefaultReelGameSessionConfig;
-import slotify4j.session.videogames.reelgames.ReelGameSessionConfig;
-import slotify4j.session.videogames.reelgames.ReelGameSessionImpl;
-import slotify4j.session.videogames.reelgames.reelscontroller.ReelGameSessionReelsController;
-import slotify4j.session.videogames.reelgames.reelscontroller.ReelGameSessionReelsControllerImpl;
-import slotify4j.session.videogames.reelgames.wincalculator.ReelGameSessionWinCalculator;
-import slotify4j.session.videogames.reelgames.wincalculator.ReelGameSessionWinCalculatorImpl;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class RandomChangeBetStrategyTest {
 
     @Test
     public void setBetForPlay() throws UnableToPlayException {
-        ReelGameSessionConfig sessionConfig = new DefaultReelGameSessionConfig();
-        ReelGameSessionReelsController reelsController = new ReelGameSessionReelsControllerImpl(sessionConfig);
-        ReelGameSessionWinCalculator winningCalculator = new ReelGameSessionWinCalculatorImpl(sessionConfig);
-        ReelGameSessionImpl session = new ReelGameSessionImpl(sessionConfig, reelsController, winningCalculator);
+        ArrayList<Long> betsDuringPlay = new ArrayList<>();
+        GameSession sessionMock = new GameSession() {
+            @Override
+            public void play() throws UnableToPlayException {
+                /* no-op */
+            }
 
-        GameSessionSimulationConfig simulationConfig = DefaultGameSessionSimulationConfig
-                .builder()
-                .withChangeBetStrategy(new RandomChangeBetStrategy())
-                .build();
-        GameSessionSimulation simulation = new GameSessionSimulationImpl(session, simulationConfig);
+            @Override
+            public long getCreditsAmount() {
+                return 1;
+            }
 
-        HashSet<Long> playedBets = new HashSet<>();
-        simulation.setBeforePlayCallback(() -> session.setCreditsAmount(1000000));
-        simulation.setAfterPlayCallback(() -> playedBets.add(session.getBet()));
+            @Override
+            public void setCreditsAmount(long creditsAmount) {
+                /* no-op */
+            }
+
+            @Override
+            public long getWinningAmount() {
+                return 0;
+            }
+
+            @Override
+            public long[] getAvailableBets() {
+                return IntStream.range(1, 100).mapToLong(Long::valueOf).toArray();
+            }
+
+            @Override
+            public boolean isBetAvailable(long bet) {
+                return false;
+            }
+
+            @Override
+            public long getBet() {
+                return 0;
+            }
+
+            @Override
+            public void setBet(long bet) {
+                if (betsDuringPlay.stream().noneMatch(value -> value == bet)) {
+                    betsDuringPlay.add(bet);
+                }
+            }
+
+            @Override
+            public boolean canPlayNextGame() {
+                return true;
+            }
+        };
+        GameSessionSimulationImpl simulation = new GameSessionSimulationImpl(
+                sessionMock,
+                DefaultGameSessionSimulationConfig
+                        .builder()
+                        .withNumberOfRounds(1000)
+                        .withChangeBetStrategy(new RandomChangeBetStrategy())
+                        .build()
+        );
 
         simulation.run();
 
-        assertEquals(playedBets.size(), session.getAvailableBets().length);
+        long[] betsArrayOfSet = betsDuringPlay.stream().mapToLong(Long::valueOf).toArray();
 
-        // After simulation playedBets should contain shuffled set of bets
-        assertFalse(
-                Arrays.equals(
-                        session.getAvailableBets(),
-                        playedBets.stream().mapToLong(Number::longValue).toArray()
-                )
+        // Contents of betsDuringPlay after simulation should contain shuffled array of all possible bets
+        assertFalse(Arrays.equals(
+                betsArrayOfSet,
+                sessionMock.getAvailableBets()
+        ));
+        assertArrayEquals(
+                Arrays.stream(betsArrayOfSet).sorted().toArray(),
+                sessionMock.getAvailableBets()
         );
-
     }
 
 }
